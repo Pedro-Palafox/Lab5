41dddbbc15a22a4b02292ade4733ecf5e993b869
Pedro Palafox <A01027841@tec.mx>
2025-03-06 20:29:30 -0600

    Lab 5.1

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,16 +1,11 @@
 import React from 'react'
 import { Outlet, NavLink } from 'react-router-dom'
+import { NavBar } from './NavBar'
 
 export const Layout = () => {
     return ( <div>
         <nav>
-            <ul>
-                <li><NavLink to="/Home">Home</NavLink></li>
-                <li><NavLink to="/Login">Login</NavLink></li>
-                <li><NavLink to="/Perfil">Perfil</NavLink></li>
-                <li><NavLink to="/Dashboard">Dashboard</NavLink></li>
-                <li><NavLink to="/NaveBar">NavBar</NavLink></li>
-            </ul>
+            <NavBar></NavBar>
         </nav>
         <hr/>
         <Outlet/>
